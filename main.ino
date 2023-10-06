42add7a4f1e82947c3edd23145da2687a952b1a1
Atheesh Thirumalairajan <github-commits@atheesh.org>
2023-10-06 11:35:14 -0400

    Drive Struct Updates

--- main.ino
+++ main.ino
@@ -1,9 +1,37 @@
+/*
+    ENES100 Autonomous Rover Project, University of Maryland
+    Copyright (C) 2023  Atheesh Thirumalairajan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "drive.h";
+
 void setup() {
-  // put your setup code here, to run once:
+  /* Define Propulsion Pins */
+  struct Propel propulsion = {
+    {10, 11}, //10LF, 11LB
+    {6, 7}, //6LF, 7LF
+    {12, 13}, //12RF, 13RB
+    {8, 9} //8RF, 9RB
+  };
 
-}
+  /* Initialize the Drive Object */
+  Drive drive(propulsion);
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  /* Never Return */
+  while(1 == 1) {
 
+  }
 }
